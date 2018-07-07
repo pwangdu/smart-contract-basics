9e4ed9192dad0e1180ccdd15ad4efb706dfe4c5a
Medlee <pankajwahane@gmail.com>
2018-07-08 02:51:13 +0530

    add a publicly accessible manager to smart contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.7;
 
 contract Lottery {
+    address public manager;
 
+    constructor() public {
+        manager = msg.sender;
+    }
 }
--- test/compile.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const { interface, bytecode } = require('../compile');
-
-describe('Compiler',() => {
-    it('must compile',() => {
-        assert.ok(interface);
-        assert.ok(bytecode);
-    })
-});
